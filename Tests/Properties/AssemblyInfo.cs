0b92d85383315bcabf12f4defc22e09ca062108b
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-13 22:34:03 -0500

    Adding tests

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d48d2490-e7f2-4ba0-bdf9-01b1e4924f50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Unit/LoggerTest.cs
@@ -0,0 +1,140 @@
+﻿using Luilliarcec.Logger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests.Unit
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void CheckThatTheLogDoesNotExist()
+        {
+            Log.Drop();
+            Assert.IsFalse(Log.Exists());
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogDoesExist()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            Assert.IsTrue(Log.Exists());
+            Log.Drop();
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogHasBeenDeleted()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            Assert.IsTrue(Log.Drop());
+        }
+
+        [TestMethod]
+        public void VerifyThatTheLogHasBeenCopiedToASpecifiedPath()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            System.IO.Directory.CreateDirectory("./test_files");
+            Assert.IsTrue(Log.Copy("./test_files"));
+            Log.Drop();
+            System.IO.Directory.Delete("./test_files", true);
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogIsSaved()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(Log.Error(ex));
+                Assert.IsTrue(Log.Warning(ex));
+                Assert.IsTrue(Log.Info(ex));
+            }
+
+            Log.Drop();
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogIsSavedAsError()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            string text = System.IO.File.ReadAllText(Log.Path);
+
+            StringAssert.Contains(text, "* Error *");
+
+            Log.Drop();
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogIsSavedAsWarning()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Warning(ex);
+            }
+
+            string text = System.IO.File.ReadAllText(Log.Path);
+
+            StringAssert.Contains(text, "* Warning *");
+
+            Log.Drop();
+        }
+
+        [TestMethod]
+        public void CheckThatTheLogIsSavedAsInfo()
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Log.Info(ex);
+            }
+
+            string text = System.IO.File.ReadAllText(Log.Path);
+
+            StringAssert.Contains(text, "* Info *");
+
+            Log.Drop();
+        }
+    }
+}
